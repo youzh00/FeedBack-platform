2bb2b5531209b9376cd770fb0dc1a3fa80aacb97
Youssef Zahi <youssef.zahi2016@gmail.com>
2022-03-04 16:34:05 +0100

    adding ratings

--- src/Component/FeedBackForm.jsx
+++ src/Component/FeedBackForm.jsx
@@ -1,40 +1,39 @@
 import React from 'react'
 import Card from './shared/Card'
 import '../index.css'
-import Button from './Button'
+import Button from './shared/Button'
 import { useState } from 'react'
+import RatingSelect from './RatingSelect'
 
 ////////////////////////////
 export default function FeedBackForm() {
   const [text,setText]=useState('')
   const [isDisabled,setIsDisabled]=useState(true)
   const [message,setMessage]=useState('')
+  const [rating,setRating]=useState()
   function handleChange(e){
 
     if(text===''){
       setIsDisabled(true)
       setMessage(null)
-      console.log('a')
     }else if(text.trim().length<10 && text!==''){
       setMessage('Text must be at least 10 characters')
       setIsDisabled(true)
-      console.log('b')
 
     }else{
       setMessage(null)
       setIsDisabled(false)
-      console.log('c')
 
     }
     setText(e.target.value)
-  
-  
-
   }
+
+  console.log(rating)
   return (
     <Card>
         <form >
-            <h2>How Would you rate your service with us</h2>
+            <div><h2>How Would you rate your service with us</h2></div>
+            <RatingSelect select={(rating)=>setRating(rating)}/>
             <div className='input-group'>
                 <input type="text" placeholder='write a review' onChange={(e)=>handleChange(e)}/>
                 <Button type={'text'} children={'Send'} isDisabled={isDisabled}/>
